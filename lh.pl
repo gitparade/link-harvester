2b87b8dadecbc2c917a2ef8f7f5a386c8e0e074c
nn <a@b.cn>
2011-09-09 17:33:11 -0500

    We use the second method

--- lh.pl
+++ lh.pl
@@ -1,11 +1,15 @@
 #!/usr/bin/env perl
 
 use strict;
+use utf8;
+use open ':encoding(utf8)';
+binmode(STDOUT, ":utf8");
 
-use CGI;
+use CGI qw/:standard :html4/;
 use CouchDB::Client;
 use WWW::Mechanize;
 use Data::Dumper;
+$CGI::DISABLE_UPLOADS = 1;
 
 # >>>>>>>>>>>>>>>>>>>>>>> Database name
 my $link_harvester_db_name = "db";
@@ -15,8 +19,8 @@ my $couchdb_uri = "http://127.0.0.1:5984/";
 my $cgi = CGI->new();
 
 print
-	$cgi->header('text/html'),
-	"<div style='z-index : 1000; position : fixed; top : 0; left : 0;'>",
+	$cgi->header(-charset=>'utf-8'),
+	"<div style='z-index : 1; position : fixed; top : 0; left : 0;'>",
 	$cgi->start_form,
 	$cgi->textfield('url'),
 	$cgi->submit(''),
@@ -24,17 +28,27 @@ print
 
 if ( $cgi->param('url') ) {
 	my $mech = WWW::Mechanize -> new(agent => "NotBlocked/0.01");
-	my $html;
+	my $http;
 	if ($cgi -> param('url') =~ /^http:\/\//) {
-		$html = $mech -> get($cgi -> param('url')) -> decoded_content;
+		$http = $cgi -> param('url');
 	} else {
-		$html = $mech -> get("http://" . $cgi -> param('url')) -> decoded_content;
+		$http = "http://" . $cgi -> param('url');
 	}
-	print $html;
+	my $html = $mech -> get($http) -> decoded_content;
+	my $text = $mech -> text();
+	&display($text);
 	# >>>>>>>>>>>>>>>>>>>>>>> CouchDB
 	my $client = CouchDB::Client -> new(uri => $couchdb_uri);
 	my $db = $client -> newDB($link_harvester_db_name);
-	my $doc = $db -> newDoc($cgi->param('url'), undef, {html => $html});
+	my $doc = $db -> newDoc($http, undef, {html => $html});
 	$doc -> create;
 	# <<<<<<<<<<<<<<<<<<<<<<< CouchDB
+}
+
+sub display {
+	foreach (1..50) {
+		printf "<div style='font-size : xx-large; z-index : 0; position : absolute; left : %s;'>", $_;
+		print $_[0];
+		print "</div>";
+	}
 }
